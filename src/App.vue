5405e1f8a391ff95a2be24e9776298f51b929034
niccoloruberti <nikiruberti@gmail.com>
2023-06-26 21:39:58 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script>
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+import '../node_modules/bootstrap/scss/bootstrap.scss';
+import axios from 'axios';
+
+export default {
+  AppHeader,
+  AppMain
+}
+</script>
+
+<template lang="">
+  <div>
+    
+  </div>
+</template>
+
+<style lang="">
+  
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,17 @@
+<script>
+import AppCard from './AppCard.vue';
+
+export default {
+    AppCard,
+}
+</script>
+
+<template lang="">
+    <div>
+        
+    </div>
+</template>
+
+<style lang="">
+    
+</style>
