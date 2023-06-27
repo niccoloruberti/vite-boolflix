e4a4a39f6e4b006bac7ddde8797fc1a99da59d3e
niccoloruberti <nikiruberti@gmail.com>
2023-06-27 16:15:58 +0200

    Milestone 2 - bandiere

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -1,5 +1,12 @@
 <script>
+import { hasFlag } from 'country-flag-icons';
+
 export default {
+    data() {
+        return {
+            hasFlag,
+        }
+    },
     props: {
         myCard: Object,
     }
@@ -11,12 +18,16 @@ export default {
         <div class="card">
             <h3>{{ myCard.title }}</h3>
             <h4>{{ myCard.original_title }}</h4>
-            <h5>{{ myCard.original_language }}</h5>
+            <img class="flag" v-if="(myCard.original_language) == 'en'" :src="`http://purecatamphetamine.github.io/country-flag-icons/3x2/GB.svg`">
+            <img class="flag" v-else-if="hasFlag(myCard.original_language.toUpperCase())" :src="`http://purecatamphetamine.github.io/country-flag-icons/3x2/${myCard.original_language.toUpperCase()}.svg`">
+            <h5 v-else>{{ myCard.original_language }}</h5>
             <h6>{{ myCard.vote_average }}</h6>
         </div>
     </div>
 </template>
 
-<style lang="">
-    
+<style lang="scss">
+    .flag {
+        width: 2rem;
+    }
 </style>
