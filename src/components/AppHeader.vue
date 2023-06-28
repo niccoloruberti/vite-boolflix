2c558fbb55fc3cb55021edd89bf45e80d1f79dda
niccoloruberti <nikiruberti@gmail.com>
2023-06-28 12:26:10 +0200

    Milstone 4 - style header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -14,12 +14,17 @@ export default {
 
 <template lang="">
     <div>
-        <input v-model="this.store.searchText" type="text">
-        <!-- emit per inviare i dati dell'input al padre -->
-        <button @click="$emit('performSearch')">Cerca</button>
+        <header class="bg-black d-flex justify-content-between align-items-center">
+                <span class="text-danger text-uppercase fs-1 fw-bold mx-3">boolflix</span>
+            <div class="mx-3">
+                <input class="rounded" v-model="this.store.searchText" type="text">
+                <!-- emit per inviare i dati dell'input al padre -->
+                <button class="btn px-1 py-0 ms-2 btn-light" @click="$emit('performSearch')">Cerca</button>
+            </div>
+        </header>
     </div>
 </template>
 
-<style lang="">
+<style lang="scss">
     
 </style>
